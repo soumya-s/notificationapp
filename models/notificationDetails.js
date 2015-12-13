9a5a51f40d73ba360f358d6e1293089f76f7359b
soumya-s <twinklesou23@gmail.com>
2015-12-13 18:20:01 +0530

    Update notificationDetails.js

--- models/notificationDetails.js
+++ models/notificationDetails.js
@@ -2,7 +2,12 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var NotificationSchema = new Schema({
-	notification :{ type: Array}
+	humanId:{ type: String},
+	updatedAt: { type: String},
+        model:{ type: String},
+        action: { type: String},
+        objectId: { type: String},
+        endpoint:{ type: String}
   
 });
 module.exports = mongoose.model('Notification', NotificationSchema);
