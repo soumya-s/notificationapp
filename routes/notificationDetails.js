9d3efe07d826a9b699152141a58b7654328e4b04
soumya-s <twinklesou23@gmail.com>
2015-12-13 19:28:40 +0530

    Update notificationDetails.js

--- routes/notificationDetails.js
+++ routes/notificationDetails.js
@@ -1,16 +1,16 @@
-
-var express   =   require('express');
+ var express   =   require('express');
 var router    =   express.Router();
 var Notification = require('../models/notificationDetails');
 
 router.post('/', function(req, res, next) {
+	    if (!error && req.statusCode == 200) {
   var notification = new Notification(req.body);
    
   notification.save(function(err, notificationResult) {
     
   });
+  }
 });
-
 
 
 module.exports = router;
